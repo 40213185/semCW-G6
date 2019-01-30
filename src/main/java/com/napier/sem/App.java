4f7a59248ac6cb2070645b72c3d468a293665554
40213185 <40213185@live.napier.ac.uk>
2019-01-30 00:54:01 +0000

    Commit 3 - added Java App which runs a simple test to see if main is running successfully.

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,10 @@
+package com.napier.sem;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        //simple test to see if main function is running
+        System.out.println("-----App.main is running successfully-----");
+    }
+}
